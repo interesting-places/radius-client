246a73a8320e1533232f7ec2c80838aec0d4c5a5
Austin Keeley <austin.keeley@gmail.com>
2014-09-27 12:53:30 -0400

    fixing push to pushobject (you idiot)

--- app/scripts/radius.js
+++ app/scripts/radius.js
@@ -1,11 +1,26 @@
+var COUCHDB_SERVER = 'http://rdius.co:5984';
+var current_blips = [];
+
+
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  // put your routes here.
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue', 'green'];
+    var allBlips = [];
+    Ember.$.getJSON(COUCHDB_SERVER + '/blip/_all_docs').then(function(data) {
+      $.each(data.rows, function(index, row) {
+        Ember.$.getJSON(COUCHDB_SERVER + '/blip/' + row.id).then(function(blip) {
+          console.log(blip);
+          allBlips.pushObject(blip);
+        });
+      });
+    });
+
+    return allBlips;
   }
 });
+
