04bae5ed30405f58eb9b0f48c077e3a59c1fd754
Austin Keeley <austin.keeley@gmail.com>
2014-09-27 18:03:23 -0400

    I LOVE YOU EMBER.JS

--- app/scripts/radius.js
+++ app/scripts/radius.js
@@ -1,10 +1,11 @@
+/* Non-ember stuff */
+
 var COUCHDB_SERVER = 'http://rdius.co:5984';
-var current_blips = [];
 
-
+/*
+ * Adds a blip to the global Google Map
+ */
 function addBlipToMap(blip) {
-  console.log(blip);
-  console.log(map);
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(blip.latitude, blip.longitude),
     map: map,
@@ -13,16 +14,34 @@ function addBlipToMap(blip) {
   });
 }
 
-App = Ember.Application.create();
+/* Begining of Ember stuff */
 
-App.Router.map(function() {
-  // put your routes here.
+Radius = Ember.Application.create();
+Radius.ApplicationManager = DS.FixtureAdapter.extend();
+
+/*
+ * The Blip model
+ * The thing that's going to make us all billionares.
+ */
+Radius.Blip = DS.Model.extend({
+  title: DS.attr('string'),
+  latitude: DS.attr('double'),
+  longitude: DS.attr('double'),
+  username: DS.attr('string'),
+  karma: DS.attr('integer')
 });
 
-App.IndexRoute = Ember.Route.extend({
+Radius.Router.map(function() {
+  this.resource('blips', { path: '/' });
+});
+
+Radius.BlipsController = Ember.ArrayController.extend({
+
+});
+
+Radius.BlipsRoute = Ember.Route.extend({
+
   model: function() {
-
-
 
     var allBlips = [];
     Ember.$.getJSON(COUCHDB_SERVER + '/blip/_all_docs').then(function(data) {
@@ -30,14 +49,30 @@ App.IndexRoute = Ember.Route.extend({
         Ember.$.getJSON(COUCHDB_SERVER + '/blip/' + row.id).then(function(blip) {
 
           addBlipToMap(blip);
-
-          console.log(blip);
           allBlips.pushObject(blip);
         });
       });
     });
-
     return allBlips;
   }
+
 });
 
+
+Radius.BlipController = Ember.ObjectController.extend({
+
+  actions: {
+
+    upvote: function() {
+      var karma = this.get('karma');
+      this.set('karma', karma + 1);
+      console.log('in upvote');
+    },
+
+    downvote: function() {
+      var karma = this.get('karma');
+      this.set('karma', karma - 1);
+      console.log('in downvote');
+    }
+  }
+});
